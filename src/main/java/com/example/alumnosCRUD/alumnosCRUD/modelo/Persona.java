4228d8012eea7f5e54886c26258c053fd31c83c6
Alejandro Couoh <alejandro.couoh@gmail.com>
2023-01-30 15:16:57 -0600

    Actualizacion 3

---
+++ src/main/java/com/example/alumnosCRUD/alumnosCRUD/modelo/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.alumnosCRUD.alumnosCRUD.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Alejandro Couoh Haas <your.name at your.org>
+ */
+@Entity
+@Table(name = "persona")
+public class Persona {
+    
+}
